3ac653b14e1624b62aa1cb90d8dd29a9ae83f4ac
Eduardo Galindo <eduardo.galindo@publicinput.com>
2026-05-12 13:16:53 -0600

    Cleanup

--- backend/tests/test_mercsoft_api.py
+++ backend/tests/test_mercsoft_api.py
@@ -5,7 +5,7 @@ import pytest
 import requests
 from pymongo import MongoClient
 
-BASE_URL = os.environ.get("REACT_APP_BACKEND_URL", "https://nextgen-dev-studio.preview.emergentagent.com").rstrip("/")
+BASE_URL = os.environ.get("REACT_APP_BACKEND_URL", "http://localhost:3000").rstrip("/")
 API = f"{BASE_URL}/api"
 
 MONGO_URL = os.environ.get("MONGO_URL", "mongodb://localhost:27017")
